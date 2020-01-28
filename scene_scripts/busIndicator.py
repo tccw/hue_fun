b65d708dea28599911bac49d2aed9971fefc7109
tccw <thomas.cowan@gmail.com>
2020-01-27 23:04:41 -0800

    Implemented basic light changing criteria.

--- scene_scripts/busIndicator.py
+++ scene_scripts/busIndicator.py
@@ -9,6 +9,11 @@ from datetime import datetime
 from google.transit import gtfs_realtime_pb2
 from phue import Bridge
 
+"""
+Route and arrival data used in this product or service is provided by permission of TransLink. 
+TransLink assumes no responsibility for the accuracy or currency of the Data used in this product or service.
+"""
+
 
 # Constants
 # TODO: fix how this behaves if a file isn't found
@@ -19,8 +24,7 @@ def load_file(path):
             d = list(map(lambda s: s.strip(), d))
             return d
     except IOError:
-        print("Error: file not found. Check that the path is correct and the file exists.")
-        sys.exit()
+        sys.exit(f"Error: file [{path}] not found. Check that the path is correct and the file exists.")
 
 
 def haversine_dist(lat1, lon1, lat2, lon2):
@@ -44,6 +48,7 @@ def haversine_dist(lat1, lon1, lat2, lon2):
     distance_m = round(distance_km * 1000, 0)
     return distance_m
 
+
 data = load_file(r"data\my_location.txt")
 lat_me = float(data[0])
 lon_me = float(data[1])
@@ -57,41 +62,54 @@ b = Bridge(bridge_ip)
 b.connect()
 
 # create a light grouping and turn then on
-lr_lamp = [1, 4]
+# lr_lamp = [1, 4]
+lr_lamp = [1]
 command = {'on': True, 'bri': 255}
 b.set_light(lr_lamp, command)
 # print(b.get_api())
 
 
 # TODO: create a function to query the API at set intervals
-feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.request.urlopen('https://gtfs.translink.ca/v2/gtfsposition?apikey=' + API_KEY)
-feed.ParseFromString(response.read())
-
-dist_list = []
-for entity in feed.entity:
-    if (entity.HasField('vehicle') and
-            (entity.vehicle.trip.route_id == "16718") and
-            (entity.vehicle.trip.direction_id == westbound)):
-        lat_1 = entity.vehicle.position.latitude
-        lon_1 = entity.vehicle.position.longitude
-        busID = entity.vehicle.vehicle.id
-        now = datetime.now()
-        bus_checkin_time = datetime.fromtimestamp(int(entity.vehicle.timestamp))
-
-        time_diff = now - bus_checkin_time
-        dist_meters = haversine_dist(lat_1, lon_1, lat_me, lon_me)
-
-        if (lon_1 > lon_me) and (dist_meters < 1300) and (dist_meters > 550):
-            dist_list.append(dist_meters)
-            print("A westbound 14 is close! Leave now!")
 
 
-        # TODO: set light color logic using traffic light system
+for n in range(500):
 
-        # print(
-        #     f'The 14 bus with ID {busID} is {dist_meters} meters away.\n'
-        #     f'GPS-data received {round(time_diff.total_seconds(), 0)} seconds ago.\n'
-        # )
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = urllib.request.urlopen('https://gtfs.translink.ca/v2/gtfsposition?apikey=' + API_KEY)
+    feed.ParseFromString(response.read())
+    green_dist = []
+    red_dist = []
 
-print(dist_list)
+    for entity in feed.entity:
+        if (entity.HasField('vehicle') and
+                (entity.vehicle.trip.route_id == "16718") and
+                (entity.vehicle.trip.direction_id == westbound)):
+            print(entity)
+            lat_1 = entity.vehicle.position.latitude
+            lon_1 = entity.vehicle.position.longitude
+            busID = entity.vehicle.vehicle.id
+            now = datetime.now()
+            bus_checkin_time = datetime.fromtimestamp(int(entity.vehicle.timestamp))
+
+            time_diff = now - bus_checkin_time
+            dist_meters = haversine_dist(lat_1, lon_1, lat_me, lon_me)
+            # TODO: set light color logic using traffic light system
+            if (lon_1 > lon_me) and (dist_meters < 1300) and (dist_meters > 500):
+                green_dist.append(dist_meters)
+            # print("A westbound 14 is close! Leave now!")
+            if lon_1 > lon_me:
+                print(
+                    f'The 14 bus with ID {busID} is {dist_meters} meters away.\n'
+                    f'GPS-data received {round(time_diff.total_seconds(), 0)} seconds ago.\n'
+                )
+    print(green_dist)
+    if not len(green_dist):
+        b.set_light(lr_lamp, 'xy', [0.6679, 0.2969])
+        print('turn red')
+    else:
+        b.set_light(lr_lamp, 'xy', [0.2206, 0.662])
+        print('turn green')
+
+    print('------------------------------------------------------------------------')
+
+    sleep(20)
