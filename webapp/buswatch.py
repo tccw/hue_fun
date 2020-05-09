6d4239c985879b7c43c15423439837d078489732
tccw <thomas.cowan@gmail.com>
2020-05-08 23:01:43 -0700

    Import busIndicator.py to run with /ifttt is visited.

--- webapp/buswatch.py
+++ webapp/buswatch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-# External listening port 9847
+from ..scene_scripts import busIndicator
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -10,8 +11,8 @@ def index():
 
 @app.route("/ifttt", methods=['GET', 'POST']) 
 def handler():
-    print("Wow Heather! You are so mgical and lovely!")
-    return "There is nothing here right now"
+    busIndicator
+    return "Huh"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0',port=8080)
